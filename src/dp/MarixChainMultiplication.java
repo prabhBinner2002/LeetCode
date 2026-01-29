eb3af4e89ab05b40705db4d5a46633df4c6cb1a8
Prabhjot Singh Binner <prabhjotsingh.binner@ucalgary.ca>
2026-01-29 14:39:53 -0700

    MCM

--- src/dp/MarixChainMultiplication.java
+++ src/dp/MarixChainMultiplication.java
@@ -37,11 +37,45 @@ public class MarixChainMultiplication {
         return ansCost;
     }
 
+    public static int tab(int[] arr) {
+        int n = arr.length;
+        int[][] dp = new int[n][n];
+
+        // Initialization
+        for (int i = 0; i < n; i++)
+            dp[i][i] = 0;
+
+        // bottom up
+        for (int len = 2; len <= n - 1; len++) {
+            for (int i = 1; i <= n - len; i++) {
+                int j = i + len - 1;
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k <= j - 1; k++) {
+                    int cost1 = dp[i][k];
+                    int cost2 = dp[k + 1][j];
+                    int cost3 = arr[i - 1] * arr[k] * arr[j];
+                    dp[i][j] = Math.min(dp[i][j], cost1 + cost2 + cost3);
+                }
+            }
+        }
+
+        for(int row[] : dp) {
+            for (int val : row) {
+                System.out.printf("%2d | ", val);
+            }
+            System.out.println();
+        }
+
+        return dp[1][n - 1];
+    }
+
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,3};
         int i = 1, j = arr.length - 1;
         int[][] dp = new int[arr.length][arr.length];
         Arrays.stream(dp).forEach(row -> Arrays.fill(row, -1));
-        System.out.println(mcmMemo(arr, i, j, dp));
+//        System.out.println(mcmMemo(arr, i, j, dp));
+
+        System.out.println(tab(arr));
     }
 }
